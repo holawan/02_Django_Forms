dce7ddc784ddcc6c28aebfaeeb38fdb788e64407
holawan <asdf134652@gmail.com>
2022-04-06 10:26:23 +0900

    form 생성하기

---
+++ articles/forms.py
@@ -0,0 +1,6 @@
+from turtle import title
+from django import forms
+
+class ArticleForm(forms.Form) :
+    title = forms.CharField(max_length=10)
+    content = forms.CharField()
---
+++ articles/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+# 제목과 내용을 누르고 제출을 했을 때 crate 변수로 가게되서 동작을 하고 마지막으로 redirect가 return 된다. 
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article(title=title, content=content)
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
