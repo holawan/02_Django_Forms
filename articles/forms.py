031c8faa286e4e2ac6cae3c32a66cd296f462a8b
holawan <asdf134652@gmail.com>
2022-04-06 11:16:10 +0900

    modelform 생성

--- articles/forms.py
+++ articles/forms.py
@@ -1,18 +1,26 @@
 from django import forms
 
-class ArticleForm(forms.Form) :
-    REGION_A = 'sl'
-    REGION_B = 'dj'
-    REGION_C = 'bs'
+from articles.models import Article
 
-    REGIONS_CHOICES = [
-        (REGION_A, '서울'),
-        (REGION_B, '대전'),
-        (REGION_C, '광주')
-    ]
+# class ArticleForm(forms.Form) :
+#     REGION_A = 'sl'
+#     REGION_B = 'dj'
+#     REGION_C = 'bs'
 
-    # Models.py와 유사한 구조 (form 클래스를 상속받음)
-    title = forms.CharField(max_length=10)
-    content = forms.CharField(widget=forms.Textarea)
-    # select 태그 만들기 
-    region = forms.ChoiceField(choices = REGIONS_CHOICES, widget = forms.Select())
+#     REGIONS_CHOICES = [
+#         (REGION_A, '서울'),
+#         (REGION_B, '대전'),
+#         (REGION_C, '광주')
+#     ]
+
+#     # Models.py와 유사한 구조 (form 클래스를 상속받음)
+#     title = forms.CharField(max_length=10)
+#     content = forms.CharField(widget=forms.Textarea)
+#     # select 태그 만들기 
+#     region = forms.ChoiceField(choices = REGIONS_CHOICES, widget = forms.Select())
+
+class ArticleForm(forms.ModelForm) :
+    class Meta :
+        model = Article
+        fields = '__all__'
+        #exclude = ('title',)
