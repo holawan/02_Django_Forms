9405067256ce9b8cee77bceed854c1fffe044c63
holawan <asdf134652@gmail.com>
2022-04-06 14:40:00 +0900

    crate 함수와 new함수 request로 구분해서 합치기

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
+    # create 이름 변경 
+    # path('new/', views.create, name='new'),
     path('create/', views.create, name='create'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
--- articles/views.py
+++ articles/views.py
@@ -11,31 +11,40 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 
-def new(request):
-    form = ArticleForm()
+# def new(request):
+#     form = ArticleForm()
+#     context = {
+#         'form' : form,
+#     }
+#     return render(request, 'articles/new.html',context)
+
+# 제목과 내용을 누르고 제출을 했을 때 crate 변수로 가게되서 동작을 하고 마지막으로 redirect가 return 된다. 
+
+# 요청에 따라 실행을 구분하고 new와 create 함수 합치기 
+def create(request):
+    if request.method == 'POST' :
+
+        form = ArticleForm(request.POST)
+        #form에서 전달한 데이터가 유효하면 저장하고 detail을 redircet해라
+        if form.is_valid() :
+            # form.save()를 하면 생성된 객체를 리턴한다.
+            article = form.save()
+            return redirect('articles:detail',article.pk)
+        #유효하지 않으면 다시 new로 가라 
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+
+        # article = Article(title=title, content=content)
+        # article.save()
+
+        # return redirect('articles:detail', article.pk)
+    elif request.method == 'GET' :
+
+        form = ArticleForm()
     context = {
         'form' : form,
     }
-    return render(request, 'articles/new.html',context)
-
-# 제목과 내용을 누르고 제출을 했을 때 crate 변수로 가게되서 동작을 하고 마지막으로 redirect가 return 된다. 
-def create(request):
-    form = ArticleForm(request.POST)
-    #form에서 전달한 데이터가 유효하면 저장하고 detail을 redircet해라
-    if form.is_valid() :
-        # form.save()를 하면 생성된 객체를 리턴한다.
-        article = form.save()
-        return redirect('articles:detail',article.pk)
-    #유효하지 않으면 다시 new로 가라 
-    return redirect('articles:new')
-    # title = request.POST.get('title')
-    # content = request.POST.get('content')
-
-    # article = Article(title=title, content=content)
-    # article.save()
-
-    # return redirect('articles:detail', article.pk)
-
+    return render(request, 'articles/create.html',context)
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
@@ -64,7 +73,9 @@ def edit(request, pk):
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
+    form = ArticleForm(request.POST,instacne=article)
+    article=form.save()
+    # article.title = request.POST.get('title')
+    # article.content = request.POST.get('content')
+    # article.save()
     return redirect('articles:detail', article.pk)
