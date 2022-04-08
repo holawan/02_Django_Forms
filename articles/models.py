d74ccd19b8ffe4e81a6ddf3238c2c9cd58f39de0
holawan <asdf134652@gmail.com>
2022-04-08 11:34:52 +0900

    image 썸네일 크기로 자동 resizing

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/',blank=True)
+    # image = models.ImageField(upload_to='images/',blank=True)
+    image = ProcessedImageField(
+        blank=True,
+        upload_to='thumbnails/',
+        processors=[Thumbnail(200, 300)],
+        format='JPEG',
+        options={'quality': 60})
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
