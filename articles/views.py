aba888a8e88647d01d071d3757032d6a6f17ff82
holawan <asdf134652@gmail.com>
2022-04-08 09:54:51 +0900

    decorator로 요청 구분 및 에러 구분

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from .forms import Article_updateForm, ArticleForm
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
 from .models import Article
 
 # Create your views here.
+@require_safe
 def index(request):
     articles = Article.objects.order_by('-pk')
     context = {
@@ -11,6 +13,7 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 # 요청에 따라 실행을 구분하고 new와 create 함수 합치기 
+@require_http_methods(['GET','POST'])
 def create(request):
     if request.method == 'POST' :
     
@@ -30,8 +33,10 @@ def create(request):
     # 유효성 검사를 통과하지 못하면 form은 에러메시지를 담고 있다. 그래서 에러메시지를 출력한다. 
     return render(request, 'articles/form.html',context)
 
+#GET과 POST만 허용한다. 아니면 405에러 리턴 
+@require_http_methods(['GET','POST'])
 def update(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article,pk=pk)
     if request.method == 'POST' : 
         # instacne=article을 안써주면 새로운 글을 만든다. 
         form = ArticleForm(request.POST,instance=article)
@@ -47,28 +52,20 @@ def update(request, pk):
     }
     return render(request, 'articles/form.html', context)
 
+@require_safe
 def detail(request, pk):
     # article = Article.objects.get(pk=pk)
+    # 객체가 있으면 객체를 없으면 404에러를 담아서 반환 
     article = get_object_or_404(Article,pk=pk)
     context = {
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
 
-
+@require_POST
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
-    if request.method == 'POST':
-        article.delete()
-        return redirect('articles:index')
-    else:
-        return redirect('articles:detail', article.pk)
+    article = get_object_or_404(Article,pk=pk)
+    article.delete()
+    return redirect('articles:index')
 
-
-# def edit(request, pk):
-#     article = Article.objects.get(pk=pk)
-#     context = {
-#         'article': article,
-#     }
-#     return render(request, 'articles/edit.html', context)
 
