8086071740f1ce80442ab97fa7a4578a000cb134
holawan <asdf134652@gmail.com>
2022-04-12 00:38:32 +0900

    signup 기능 만들기

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 # Create your views here.
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_http_methods,require_POST
+
 
 @require_http_methods(['GET','POST'])
 def login(request) :
@@ -34,3 +35,17 @@ def logout(request):
     if request.user.is_authenticated : 
         auth_logout(request)
     return redirect('articles:index')
+
+def signup(request) :
+    if request.method == 'POST' :
+        form=UserCreationForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return redirect('articles:index')
+    else :
+        form = UserCreationForm()
+        
+    context = {
+        'form' :form,
+    }
+    return render(request, 'accounts/signup.html',context)
