3db5c2a10161ce10bf7db1fc1bc4633bdcd74cf0
holawan <asdf134652@gmail.com>
2022-04-13 12:11:47 +0900

    댓글 클래스 추가

--- articles/models.py
+++ articles/models.py
@@ -18,3 +18,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+#댓글 
+class Comment(models.Model) :
+    #게시글 참조 on_delete를 이용해 게시글이 삭제되면 댓글도 자동 삭제되게 설정
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self) :
+        return self.content
