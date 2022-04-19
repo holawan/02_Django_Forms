9c318e3ba2d646f226b3777644d78da73c13cbaa
holawan <asdf134652@gmail.com>
2022-04-19 09:46:26 +0900

    [ADD] follow

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser) :
-    pass
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
